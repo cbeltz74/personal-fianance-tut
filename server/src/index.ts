7bf4b61c2c38dedd1a1462afac69b89ea3c2014c
Colin Beltz <colinbeltz@gmail.com>
2024-05-02 15:53:34 -0500

    Created connection to MongoDB

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+// password: kc0kyypfS3GDpJUy
+
+import express, { Express } from 'express';
+import mongoose from 'mongoose';
+
+const app: Express = express(); 
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+const mongoURI: string = "mongodb+srv://colinbeltz:FW4rowAVX3jT5S2V@personalfinancetracker.wmpsior.mongodb.net/";
+
+mongoose.connect(mongoURI)
+.then(() => console.log("CONNECTED TO MONGODB!"))
+.catch((err) => console.error("Failed to Connect to MongoDB:", err));
+
+app.listen(port, () => {
+    console.log(`Server running on Port ${port}`)
+});
